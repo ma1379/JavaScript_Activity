cbad678bc772103a852ee2c5f8aae75d0b358cb2
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-15 13:50:15 -0500

    Java Script Practice

---
+++ jsPractice.js
@@ -0,0 +1,9 @@
+var l = prompt("What is the height of the box")
+var w = prompt("What is the height?")
+var h = prompt("What is the height?")
+var length = parseInt(l)
+var width = parseInt(w)
+var height = parseInt(h)
+
+var volume = (length*width*height)
+console.log(volume)
