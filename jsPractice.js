bc9ff5ff3e2c8687b229d810d649ab4940dbd04c
Sanjay Murali <muralisanjay961@gmail.com>
2022-06-15 14:04:23 -0500

    volume of box

---
+++ jsPractice.js
@@ -0,0 +1,9 @@
+let length = prompt("length of box?")
+let width = prompt("width of box?")
+let height = prompt("height of box?")
+
+length = parseFloat(length)
+width = parseFloat(width)
+height = parseFloat(height)
+
+console.log(length*width*height + " is the volume of the box");
