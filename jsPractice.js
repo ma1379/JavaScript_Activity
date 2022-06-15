775b7d637f18eae12ee592eb9d2ea977462a795e
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-15 14:18:17 -0500

    test averages activity

--- jsPractice.js
+++ jsPractice.js
@@ -1,4 +1,20 @@
-length = prompt("What is the length of the box?")
-width = prompt("What is the width of the box?")
-height = prompt("What is the height of the box?")
-console.log("The volume of the box is " + (length * width * height))
+let grade1 = prompt("Grade of test 1")
+let grade2 = prompt("Grade of test 2")
+let grade3 = prompt("Grade of test 3")
+grade1 = parseFloat(grade1)
+grade2 = parseFloat(grade2)
+grade3 = parseFloat(grade3)
+let letterGrade = ""
+let average = (grade1 + grade2 + grade3) / 3
+if(average >= 92.5) {
+    letterGrade = "A"
+} else if(average >= 84.5) {
+    letterGrade = "B"
+} else if(average >= 77.5) {
+    letterGrade = "C"
+} else if(average >= 69.5) {
+    letterGrade = "D"
+} else{
+    letterGrade = "F"
+}
+console.log("Your average test score is " + average + " and your letter grade is " + letterGrade + ".")
