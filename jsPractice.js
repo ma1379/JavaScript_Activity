a13b5db6a64bb4abd8a7973e0edd31f71fec693c
Surya Arvind <24arvins@elmbrookstudents.org>
2022-06-15 14:04:59 -0500

    Volume Activity

---
+++ jsPractice.js
@@ -0,0 +1,5 @@
+let length=parseFloat(prompt("What is the length of the box"));
+let width=parseFloat(prompt("What is the width of the box"));
+let height=parseFloat(prompt("What is the height of the box"));
+let volume=length*width*height;
+console.log("The volume of the box is "+volume);
