bdcf39f154cfe4a6914da875310a18a1cc33aa1f
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-15 14:04:16 -0500

    javascript practice

---
+++ jsPractice.js
@@ -0,0 +1,4 @@
+length = prompt("What is the length of the box?")
+width = prompt("What is the width of the box?")
+height = prompt("What is the height of the box?")
+console.log("The volume of the box is " + (length * width * height))
