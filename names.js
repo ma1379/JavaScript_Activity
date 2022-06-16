acc867c527f873b6caaee379d3f55afe333c502a
Sanjay Murali <muralisanjay961@gmail.com>
2022-06-16 10:03:28 -0500

    finished v2 of longest last name

--- names.js
+++ names.js
@@ -8,16 +8,29 @@ for (let i = 0; i < people; i++) {
     let names = prompt("name of person " + (i+1) + "?");
     peopleNames.push(names);
 }
+function longestLastName(people) {
 for (let count = 0; count < people; count++) {
     biggest = peopleNames[0];
-    smallest = peopleNames[1];
     if (peopleNames[count].length > biggest.length) {
         biggest = peopleNames[count];
     }
-    if (peopleNames[count].length < smallest.length) {
-        smallest = peopleNames[count];
+    return biggest;
+}
+}
+
+function smallestLastName(people) {
+    for (let count = 0; count < people; count++) {
+        smallest = peopleNames[1];
+        if (peopleNames[count].length < smallest.length) {
+            smallest = peopleNames[count];
+        }
+        return smallest;
     }
 }
+
+biggest = peopleNames.map(longestLastName)
+smallest = peopleNames.map(smallestLastName)
+
 
 console.log("Longest name" + biggest)
 console.log("Range of lengths: " + smallest + " to " + biggest)
