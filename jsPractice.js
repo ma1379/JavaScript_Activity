7ac5e6bb552ebe811b0219dfc084b839d4f99d33
Brandon Moyer <etaleocallidum@gmail.com>
2022-06-16 10:05:07 -0500

    finished v2 of names activity
    i think it's messier with functions, but alright lol

--- jsPractice.js
+++ jsPractice.js
@@ -8,26 +8,33 @@ fullNames.push(personName)
 let commaIndex = personName.lastIndexOf(',')
 lastNames.push(personName.substring(commaIndex+2))
 }
-let longestNameLength = 0
-let longestLastNameLength = 0;
+
+function longestLastName(lastName, fullName) {
+let longestLastNameLength = 0
 let longestLastNameIndex = -1
-let shortestNameLength = 9999
 for(let i = 0; i < numPeople; i++) {
-    if(lastNames[i].length > longestLastNameLength) {
-        longestLastNameLength = lastNames[i].length
+    if(lastName[i].length > longestLastNameLength) {
+        longestLastNameLength = lastName[i].length
         longestLastNameIndex = i
     }
 }
-console.log("Full name with the longest last name: " + fullNames[longestLastNameIndex])
+return fullName[longestLastNameIndex]
+}
+function nameRange(fullName) {
+let longestNameLength = 0
+let shortestNameLength = 9999
 for(let i = 0; i < numPeople; i++) {
-    if(fullNames[i].length < shortestNameLength) {
+    if(fullName[i].length < shortestNameLength) {
         shortestNameLength = fullNames[i].length
     }
-    if(fullNames[i].length > longestNameLength) {
+    if(fullName[i].length > longestNameLength) {
         longestNameLength = fullNames[i].length
     }
 }
-console.log("Range of name lengths: " + (longestNameLength - shortestNameLength))
+return (longestNameLength - shortestNameLength)
+}
+console.log("Full name with the longest last name: " + longestLastName(lastNames, fullNames))
+console.log("Range of name lengths: " + nameRange(fullNames))
 for(let i = 0; i < numPeople; i++) {
     console.log(fullNames[i])
 }
